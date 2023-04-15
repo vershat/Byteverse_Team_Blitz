f04c8432eb7157144f2c681295edc56382a90e56
KarnikaKhandelwal <karnika.khandelwal.19@gmail.com>
2023-04-15 19:47:29 +0530

    Added Splash Screen

---
+++ Thella wala/thella_wala/lib/splash_screen/Screen1.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Screen1 extends StatefulWidget {
+  @override
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0x0088ffdd),
+                Color(0x00ffffff),
+              ]),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                const Text(
+                  "Gali_Food",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                  ),
+                ),
+                Image.asset(
+                  "lib/assets/thela.png",
+                  height: 300.0,
+                  width: 300.0,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      // foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: const Color(0xff88FFDD)),
+
+                  // ignore: prefer_const_constructors
+                  onPressed: () {
+                    // Navigator.push(context, MaterialPageRoute(builder: (context) => const EnterMobileNo()),);
+                    },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 34),
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  // onPressed: () {},
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
