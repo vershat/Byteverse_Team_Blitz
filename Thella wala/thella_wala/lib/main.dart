8454367c47f9988143bab29c4cb4642c82331617
KarnikaKhandelwal <96221811+KarnikaKhandelwal@users.noreply.github.com>
2023-04-16 19:26:11 +0530

    update main.dart

--- Thella wala/thella_wala/lib/main.dart
+++ Thella wala/thella_wala/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: GetUserLoc(),
+      home: Screen1(),
       debugShowCheckedModeBanner: false,
     );
   }
